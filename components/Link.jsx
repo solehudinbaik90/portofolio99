1d7bf52e024cdb2288aa97a62bd2b1f48efada03
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-30 19:56:34 +0700

    Enhance Link component to handle file downloads

--- components/Link.jsx
+++ components/Link.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import NextLink from 'next/link'
 
-const Link = ({ href, children, ...props }) => {
+const isFileLink = (href) => href?.startsWith('/') && /\.[a-zA-Z0-9]+$/.test(href)
+
+const Link = ({ href, children, legacyBehavior, passHref, download, ...props }) => {
   const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'))
+  const isFile = isFileLink(href)
+
+  if (isFile) {
+    const fileProps = { href, download: download ?? true, ...props }
+
+    return legacyBehavior && React.isValidElement(children) ? (
+      React.cloneElement(children, fileProps)
+    ) : (
+      <a {...fileProps}>{children}</a>
+    )
+  }
 
   return isInternalLink ? (
-    <NextLink href={href} {...props}>
+    <NextLink href={href} passHref={passHref} legacyBehavior={legacyBehavior} {...props}>
       {children}
     </NextLink>
   ) : (
