5144281834cc73156662da7df44ab305a27ccf76
Muhamad Soleh <solehudinz3692@gmail.com>
2026-08-30 12:20:03 +0700

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/**
+ * Sitemap is generated via the next-sitemap plugin. For more options see:
+ * https://www.npmjs.com/package/next-sitemap
+ */
+
+module.exports = {
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'),
+  generateRobotsTxt: true,
+  // ...other options
+}
---
+++ theme.config.js
@@ -0,0 +1,130 @@
+/** *************************************************************
+ * Please refer to the Theme Options section in documentation   *
+ ****************************************************************/
+
+/**
+ * Icons from react-icons: https://react-icons.github.io/react-icons
+ */
+
+import { IoLogoTwitter, IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io5'
+import { TfiHome, TfiPencilAlt } from 'react-icons/tfi'
+import { SlUser, SlBriefcase, SlEnvelope, SlTrophy } from 'react-icons/sl'
+
+/**
+ * Main Menu Items
+ */
+
+export const menu = [
+  {
+    name: 'Beranda',
+    slug: '/',
+    Icon: TfiHome,
+  },
+  {
+    name: 'Profil',
+    slug: '/about',
+    Icon: SlUser,
+  },
+  {
+    name: 'Layanan',
+    slug: '/services',
+    Icon: SlBriefcase,
+  },
+  {
+    name: 'Artikel',
+    slug: '/blog',
+    Icon: TfiPencilAlt,
+  },
+  {
+    name: 'Proyek',
+    slug: '/projects',
+    Icon: SlTrophy,
+  },
+  {
+    name: 'Contact',
+    slug: '/contact',
+    Icon: SlEnvelope,
+  },
+]
+
+/**
+ * Social Links under the Main Menu
+ */
+
+export const social = [
+  {
+    name: 'Twitter',
+    url: 'https://www.twitter.com/',
+    Icon: IoLogoTwitter,
+  },
+  {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/',
+    Icon: IoLogoInstagram,
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/',
+    Icon: IoLogoLinkedin,
+  },
+]
+
+/**
+ * General configurations
+ */
+
+export const config = {
+  dateLocale: 'en-US',
+  dateOptions: {
+    // dateOptions is passed to JavaScript's toLocaleDateString()
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC',
+  },
+  convertKit: {
+    tipUrl: 'https://fantastic-mover-3439.ck.page/products/blog',
+  },
+  contactForm: {
+    inputs: require('./content/contact-form.json'),
+    recipient: 'RECIPIENT@EXAMPLE.com',
+    sender: 'SENDER@EXAMPLE.com',
+    subject: 'EMAIL NOTIFICATION SUBJECT',
+  },
+}
+
+/**
+ * MDX/Markdown configurations
+ */
+
+export const mdxConfig = {
+  publicDir: 'public',
+  pagesDir: 'content',
+  fileExt: '.md',
+  collections: ['/blog', '/projects'],
+  remarkPlugins: [],
+  rehypePlugins: [],
+}
+
+/**
+ * Global SEO configuration for next-seo plugin
+ * https://github.com/garmeeh/next-seo
+ */
+
+export const siteMetaData = {
+  siteUrl:
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000'),
+  authorName: 'Muhamad Soleh',
+  siteName: 'Muhamad Soleh',
+  defaultTitle: 'Muhamad Soleh Portofolio',
+  titleTemplate: 'Muhamad Soleh | %s',
+  description: 'Portofolio Muhamad Soleh Menampilkan CV dan Proyek.',
+  email: 'solehudinz3692@gmail.com',
+  locale: 'en_US',
+  twitter: {
+    handle: '@handle',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+}
