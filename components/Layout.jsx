dde700952f8864ca14e5423fc73ce736a57e18ed
Muhamad Soleh <solehudinz3692@gmail.com>
2026-09-01 09:06:26 +0700

    Add left padding to Layout component
    Added padding to the left of the layout for better spacing.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -19,7 +19,7 @@ const Layout = (props) => {
       <ThemeSwitcher setTheme={setTheme} />
       <div className="fixed bottom-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
       <div className="fixed top-0 left-0 z-50 hidden w-full bg-black md:block lg:h-4 xl:h-8"></div>
-      <div className="relative w-full max-w-screen-xl">
+      <div className="relative w-full max-w-screen-xl md:pl-16">
         <main className="relative h-full bg-gradient-to-tr from-omega-900 via-omega-900 to-omega-800">
           <div className="flex h-full w-full flex-col content-center items-center pb-20 lg:py-4 xl:py-8">
             {children}
